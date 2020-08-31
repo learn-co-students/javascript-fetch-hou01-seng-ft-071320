7bafaadf72ff6cb3539ecacd4e53609a2181f286
JuanToanXoup <Johndoan42@gmail.com>
2020-08-31 11:55:57 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+
+const token = 'bddcca1e1b912562de805dbbbd3c3c75961273f0';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
